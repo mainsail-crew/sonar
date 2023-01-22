7cb96c2ea0de28ed1a36b3fc995b7bcca24017e3
Stephan Wendel <me@stephanwe.de>
2023-01-22 15:33:38 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/get_config.sh
+++ libs/get_config.sh
@@ -18,8 +18,27 @@
 
 
 function get_config_path {
-    time echo "sourced $(basename "$(caller)")"
+    local dot_config_file old_path new_path
+    dot_config_file="${BASE_SNR_PATH}/tools/.config"
+    old_path="${BASE_USER_HOME}/klipper_config/sonar.conf"
+    new_path="${BASE_USER_HOME}/printer_data/sonar.conf"
+
+    if [[ -f "${dot_config_file}" ]]; then
+        # shellcheck disable=SC1090
+        source "${dot_config_file}"
+        if [[ -f "${SONAR_CONFIG_PATH}" ]]; then
+            echo "${SONAR_CONFIG_PATH}"
+        fi
+        return
+    fi
+    if [[ -f "${new_path}" ]] && [[ ! -h "${new_path}" ]]; then
+        echo "${new_path}"
+        return
+    fi
+    if [[ -f "${old_path}" ]] && [[ ! -f "${new_path}" ]]; then
+        echo "${old_path}"
+        return
+    fi
 }
 
-
-get_config_path
+debug_msg "Config File location: $(get_config_path)"
