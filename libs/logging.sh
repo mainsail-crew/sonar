447b02995447d6aaf7458de589e36121345c67f6
Stephan Wendel <me@stephanwe.de>
2023-01-24 17:49:22 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/logging.sh
+++ libs/logging.sh
@@ -38,7 +38,7 @@ function log_msg {
         prefix="$(date +'[%D %T]') sonar:"
         echo -e "${prefix} ${msg}" | tr -s ' ' >> "${SNR_LOG_PATH}" 2>&1
     fi
-    logger -t sonar "${msg}" && wait "$(pidof logger)"
+    logger -t sonar "${msg}" & wait "${!}"
 }
 
 function print_cfg {
