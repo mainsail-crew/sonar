3378182806e104d5197cacf711728feab4bb505d
Stas Yakobov <16795594+fire1ce@users.noreply.github.com>
2023-01-25 22:22:59 +0200

    fix: add symlink check to create_filestructure() (#8)
    * added [[ ! -L "${i}" ]] - symlink check to create_filestructure()
    
    since a common use is to symlinking a a config folder in order to backup it in many ways like git. I've added [[ ! -L "${i}" ]] - symlink check to create_filestructure() it will check if dir is missing or if it's not a symlink before trying to execute mkdir -p. 
    since your only check was to check if its a Dir your install fails on mkdir -p since its already exists as symlink at my case.
    
    * added sudo -u "${BASE_USER}"  for creating the config folder
    
    also if a missing dir you create it as root user. this will fail the install script at line 159 for wrong permissions

--- tools/install.sh
+++ tools/install.sh
@@ -113,7 +113,8 @@ import_config() {
 
 create_filestructure() {
     for i in "${SONAR_CONFIG_PATH}" "${SONAR_LOG_PATH%/*.*}"; do
-        if [[ ! -d "${i}" ]]; then
+        if [[ ! -d "${i}" ]] && [[ ! -L "${i}" ]]; then
+            sudo -u "${BASE_USER}" \
             mkdir -p "${i}"
         fi
     done
